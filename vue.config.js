8f9c70285257acff234740dbd0be4800c97ee0f9
Sylvain GUILLEMIN <sylvain.guillemin@cea.fr>
2023-12-27 17:24:43 +0100

    v6

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    // Configuration spécifique du Vue CLI
-    devServer: {
-      port: 8080, // Port pour le serveur de développement
-      proxy: 'http://localhost:3000' // Configuration du proxy pour le développement
-    },
-    // Configuration pour les options de production
-    productionSourceMap: false, // Désactive les source maps en production
-    // Autres configurations personnalisées...
     publicPath: '/vueapp01/',
     baseUrl: '/vueapp01/'
   };
